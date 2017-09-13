872806dda2b15c0271ac82d2962379ab40a96e0f
alfie junior <rockefellerthe2nd@gmail.com>
2017-09-12 22:41:09 -0400

    QueryUtil.java is added

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -32,23 +32,21 @@ public class EarthquakeActivity extends AppCompatActivity {
         setContentView(R.layout.earthquake_activity);
 
         // Create a fake list of earthquake locations.
-        ArrayList<Earthquake> earthquakes = new ArrayList<>();
+        /*ArrayList<Earthquake> earthquakes = new ArrayList<>();
         earthquakes.add(new Earthquake("7.2", "San Francisco", "Feb 2, 2016"));
         earthquakes.add(new Earthquake("6.1", "London", "July 20, 2015"));
         earthquakes.add(new Earthquake("3.9", "Tokyo", "Nov 10, 2014"));
         earthquakes.add(new Earthquake("5.4", "Mexico City", "May 3, 2014"));
         earthquakes.add(new Earthquake("2.8", "Moscow", "Jan 31, 2013"));
         earthquakes.add(new Earthquake("4.9", "Rio de Janeiro", "Aug 19, 2012"));
-        earthquakes.add(new Earthquake("1.6", "Paris", "Oct 30, 2011"));
+        earthquakes.add(new Earthquake("1.6", "Paris", "Oct 30, 2011"));*/
 
+        ArrayList<Earthquake> earthquakes = QueryUtil.extractEarthquakes();
 
-        // Create an {@link EarthquakeAdapter}, whose data source is a list of
-        // {@link Earthquake}s. The adapter knows how to create list item views for each item
-        // in the list.
+        ListView listView = (ListView) findViewById(R.id.list);
+
         EarthquakeAdapter quakeAdapter = new EarthquakeAdapter(this, earthquakes);
 
-        // Get a reference to the ListView, and attach the adapter to the listView.
-        ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(quakeAdapter);
 
     }
